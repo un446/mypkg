af16977d4b4afcf39886166559bc0fd60f8493e6
Yuna Isomura <yuna.i.200446@gmail.com>
2024-11-22 02:16:18 +0900

    Add talker and listener packages

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,17 +1,36 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.srv import Query
 
 
 rclpy.init()
 node = Node("listener")
 
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
-
-
 def main():
-    pub = node.create_subscription(Int16, "countup", cb, 10)
-    rclpy.spin(node)
+    client = node.create_client(Query, "query") #サービスのクライアントの作成
+    while not client.wait_for_service(timeout_sec=1.0): #サービスの待ち待ち
+        node.get_logger().info('待機中')
+
+    req = Query.Request()
+    req.name = "磯村結奈"
+    future = client.call_async(req) #非同期でサービスを呼び出し
+
+
+    while rclpy.ok():
+        rclpy.spin_once(node) #一回だけサービスを呼び出したら終わり
+        if future.done():     #終わっていたら
+            try:
+                response = future.result() #結果を受取り
+            except:
+                node.get_logger().info('呼び出し失敗')
+            else: #このelseは「exceptじゃなかったら」という意味のelse
+                node.get_logger().info("age: {}".format(response.age))
+
+            break #whileを出る
+
+    node.destroy_node() #ノードの後始末   （後始末は無限ループでないので書きました。
+    rclpy.shutdown()    #通信の後始       ただ、本来は無限ループでも書いたほうがよいです。）
+
+if __name__ == '__main__':
+    main()
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,21 +1,21 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.srv import Query
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
 
 
-def cb():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
+def cb(request, response):
+    if request.name == "磯村結奈":
+        response.age = 20
+
+    else:
+        response.age = 255
+
+    return response
 
 
 def main():
-    node.create_timer(0.5, cb)
+    srv = node.create_service(Query, "query", cb)
     rclpy.spin(node)
