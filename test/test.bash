52f87c63ddbc1f41a6350018df9cb6d8e54b95f2
Yuna Isomura <yuna.i.200446@gmail.com>
2025-01-04 23:53:46 +0900

    Add

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,30 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+ng() {
+    echo "NG at Line $1"
+    res=1
+}
+
+res=0
+
+{
+ros2 run mypkg system_status_publisher
+}&
+{
+  sleep 10
+  echo ""
+  system_status_log=$(ros2 topic echo /system_ststus--once)
+  echo "$system_status_log"
+
+  echo "$system_status_log" | grep -E 'CPU:' || ng ${LINENO}
+  echo "$system_status_log" | grep -E 'Memory:' || ng ${LINENO}
+  echo "$system_status_log" | grep -E 'Disk:' || ng ${LINENO}
+}
+echo ""
+
+sleep 10
+
+[ "$res" = 0 ] && echo "OK"
+exit $res
